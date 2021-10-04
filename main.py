83fbe575001ee85a06a3544057c08a2781e97a15
pythontest <pythontest0024@gmail.com>
2021-10-04 22:37:05 +0530

    Made significant changes in design. Changed design of the cards at main page showing the section lists nicely. Made a utility function which can be used to treat text in jinja. Trying to keep theme as black and white. Added a picture from unsplash. Discovered that images can be used from unsplash along with a (really)large variety of modifications e.g. clipping, overlaying, face-detection, edge detection really great utility.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 import re
@@ -20,29 +20,42 @@ class Section(db.Model):
 db.create_all()
 
 
-def treat_section_text(text, sec_num):
-    final_text = text
-    final_text = final_text.replace("\n", "<br/><br/>")
-    regex = fr"^.*{sec_num}\.\s"
-    final_text = re.sub(regex, "", final_text, 1)
-    return final_text
+@app.context_processor
+def utility_processor():
+    def treat_section_text(text, sec_num):
+        final_text = text
+        final_text = final_text.replace("\n", "<br/><br/>")
+        regex = fr"^.*{sec_num}\.\s"
+        final_text = re.sub(regex, "", final_text, 1)
+        return final_text
+
+    return dict(treat_section_text=treat_section_text)
+
+
+# def treat_section_text(text, sec_num):
+#     final_text = text
+#     final_text = final_text.replace("\n", "<br/><br/>")
+#     regex = fr"^.*{sec_num}\.\s"
+#     final_text = re.sub(regex, "", final_text, 1)
+#     return final_text
 
 
 @app.route("/")
 def home():
+    return redirect(url_for("act_list", page_num=1))
     return render_template("index.html")
 
-@app.route("/income-tax-act/<page-num>")
-def act_list():
-    sections = Section.query.filter_by(section_number=section_num).first()
-    return render_template("index.html")
+
+@app.route("/income-tax-act/<page_num>")
+def act_list(page_num):
+    sections = Section.query.filter(Section.id < 10).all()
+    return render_template("index.html", sections=sections)
+
 
 @app.route("/section/<section_num>")
 def section_view(section_num):
     section = Section.query.filter_by(section_number=section_num).first()
-    text = treat_section_text(text=section.section_text, sec_num=section_num)
-
-    return render_template("section_view.html", section=section, text=text)
+    return render_template("section_view.html", section=section)
 
 
 if __name__ == "__main__":
